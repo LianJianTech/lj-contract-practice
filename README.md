fa948ad6b9cfabcada8dc460143a53b6df22d422
vincecfl <vincecfl@gmail.com>
2022-05-16 10:48:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# flash meta contract practice(hardhat)
+
+## 准备环境
+- 安装node
+- 安装npm
+- 安装solc
+- 切换solc版本
+- alchemy api
+- etherscan api
+
+## 创建项目
+```
+(Create a basic sample project)
+
+mkdir lj-contract-practice && cd lj-contract-practice
+npm init --yes && npm install hardhat && npx hardhat
+
+✔ Do you want to add a .gitignore? (Y/n) · y
+? Do you want to install this sample project's dependencies with npm (@nomiclabs/hardhat-waffle ethereum-waffle chai @nomiclabs/hardhat-ethers ethers)? (Y/n) › y
+
+
+npm install dotenv
+npm install --save-dev hardhat-gas-reporter 
+npm install --save-dev @nomiclabs/hardhat-etherscan
+```
+
+## hardhat命令
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/deploy.js
+npx hardhat help
+```
+
+## 功能
+```
+编译
+测试
+部署
+验证
+调用-读操作
+调用-写操作
+升级合约
+多重签名
+```
+
+## 查看地址拥有的ETH数量
+```
+npx hardhat balance --account 0x833dE082e21E1250fc112F2654E2441052ca28fB
+```
+
+## 编译合约
+```
+npx hardhat compile
+```
+
+## 部署合约
+```
+npx hardhat run scripts/deploy.js --network kovan 
+hash值: 0xcdf7e163f56c2507914eb3284ea7d7132cfc857a44b2a2b73f4edc69d2fdafaf
+合约地址: 0x7c269bEe4773B4E6B8F408ABdd5c01788C76C871
+ ```
+
+## 验证合约
+```
+npx hardhat verify --network kovan 0x7c269bEe4773B4E6B8F408ABdd5c01788C76C871 "HelloWorld"
+```
+
+## 部署合约-读操作
+```
+npx hardhat greet --contract 0x7c269bEe4773B4E6B8F408ABdd5c01788C76C871 
+```
+
+## 部署合约-写操作
+```
+npx hardhat setGreeting --contract 0x7c269bEe4773B4E6B8F408ABdd5c01788C76C871 --value "LianJian Tech"
+```
---
+++ hardhat.config.js
@@ -0,0 +1,84 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
+
+
+const defaultNetwork = 'kovan';
+
+// npx hardhat accounts
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// npx hardhat balance --account 0x833dE082e21E1250fc112F2654E2441052ca28fB
+task("balance", "prints an account's balance")
+    .addParam("account", "the account's address")
+    .setAction(async taskArgs => {
+      await ethers.provider.getBalance(taskArgs.account).then((balance) => {
+        // 余额是 BigNumber (in wei); 格式化为 ether 字符串
+        let etherString = ethers.utils.formatEther(balance);
+
+        console.log("Balance: " + etherString);
+      });
+    });
+
+
+
+const  greeter_json = require('./artifacts/contracts/Greeter.sol/Greeter.json');
+const greeter_abi = greeter_json.abi;
+
+// npx hardhat greet --contract 0x18EeafE5d8AF4279274de1d4aA4BBE98bD15a0bA
+task("greet", "")
+    .addParam("contract", "")
+    .setAction(async taskArgs => {
+      let contractAddress = taskArgs.contract;
+      let provider = ethers.getDefaultProvider(defaultNetwork);
+      let contract = new ethers.Contract(contractAddress, greeter_abi, provider);
+      let currentValue = await contract.greet();
+      console.log("greet currentValue:%s", currentValue);
+    });
+
+
+// npx hardhat setGreeting --contract 0x18EeafE5d8AF4279274de1d4aA4BBE98bD15a0bA --value "您好，世界"
+task("setGreeting", "")
+    .addParam("contract", "")
+    .addParam("value", "")
+    .setAction(async (taskArgs) => {
+      let contractAddress = taskArgs.contract;
+      let privateKey = process.env.KOVAN_PRIVATE_KEY;
+      let provider = ethers.getDefaultProvider(defaultNetwork);
+      let contract = new ethers.Contract(contractAddress, greeter_abi, provider);
+      let wallet = new ethers.Wallet(privateKey, provider);
+      let contractWithSigner = contract.connect(wallet);
+      let tx = await contractWithSigner.setGreeting(taskArgs.value);
+      console.log("setGreeting hash:%s", tx.hash);
+      let currentValue = await contract.greet();
+      console.log("setGreeting currentValue:%s", currentValue);
+    });
+
+
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  gasReporter: {
+    enabled: false
+  },
+  defaultNetwork: defaultNetwork,
+  /*etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },*/
+  networks: {
+    kovan: {
+      url: `https://eth-kovan.alchemyapi.io/v2/` + process.env.ALCHEMY_API_KEY,
+      accounts: [`0x`+process.env.KOVAN_PRIVATE_KEY]
+    }
+  },
+};
