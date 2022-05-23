7a9faadd738247be91bd6965d581bd0c38b57b2d
vincecfl <vincecfl@gmail.com>
2022-05-23 22:11:34 +0800

    adjust gnosis safe

---
+++ gnosis_safe/hardhat.config.js
@@ -0,0 +1,52 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
+
+
+// hardhat kovan rinkeby
+const defaultNetwork = 'hardhat';
+
+// npx hardhat accounts
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// npx hardhat balance --account 0x833dE082e21E1250fc112F2654E2441052ca28fB --network rinkeby
+task("balance", "prints an account's balance")
+    .addParam("account", "the account's address")
+    .setAction(async taskArgs => {
+      await ethers.provider.getBalance(taskArgs.account).then((balance) => {
+        // 余额是 BigNumber (in wei); 格式化为 ether 字符串
+        let etherString = ethers.utils.formatEther(balance);
+
+        console.log("Balance: " + etherString);
+      });
+    });
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  gasReporter: {
+    enabled: false
+  },
+  hardhat: {
+    allowUnlimitedContractSize: false,
+  },
+  defaultNetwork: defaultNetwork,
+  etherscan: {
+    apiKey: process.env.etherscan_api_key
+  },
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/` + process.env.rinkeby_alchemy_api_key,
+      accounts: [`0x` + process.env.private_key]
+    }
+  },
+};
---
+++ gnosis_safe/scripts/deploy.box.v2.proxy.js
@@ -0,0 +1,22 @@
+const hre = require("hardhat");
+const {upgrades} = require("hardhat");
+
+async function main() {
+    console.log("Upgrading Box to BoxV2...");
+    const proxyAddress = "0x40a5e3a689bd3A2bb3284020c33C361970dB529A";
+    const BoxV2 = await hre.ethers.getContractFactory("BoxV2");
+
+    //upgradeProxy
+    const boxV2 = await upgrades.upgradeProxy(proxyAddress, BoxV2);
+    // 此处为BoxV2的Proxy地址 不是BoxV2的
+    console.log("BoxV2 deployed to:", boxV2.address);
+    console.log("Upgrading Box to BoxV2 success");
+
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
